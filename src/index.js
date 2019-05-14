441e7b28ae4a8919c9fdaccf7ae88b77d605decd
Ricll <ricllima@hotmail.com>
2019-05-13 22:26:08 -0300

    index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,42 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
+import PropTypes from "prop-types";
 
 class Button extends Component {
+  static defaultProps = {
+    children: "Salvar"
+  };
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
+
   render() {
-    return <a href="">Salvar</a>;
+    return (
+      <button onClick={this.props.onClick} style={{ bbuttonckground: "#F00" }}>
+        {this.props.children}
+      </button>
+    );
   }
 }
 
 class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
   render() {
     return (
       <div>
         <h1>Hello Word</h1>
-        <Button />
+        <h2>{this.state.counter}</h2>
+        <Button onClick={() => alert("PropTypes")} />
+        <Button onClick={this.handleClick}>Somar!!!</Button>
       </div>
     );
   }
